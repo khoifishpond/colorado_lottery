d1e6a0462e5086c5761dc662233042e196791c94
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-24 15:30:06 -0700

    Add has a full name test

--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -15,5 +15,15 @@ class ContestantTest < Minitest::Test
     assert_instance_of(Contestant, alexander)
   end
 
-  
+  def test_has_a_full_name
+    alexander = Contestant.new({
+      first_name: 'Alexander',
+      last_name: 'Aigiades',
+      age: 28,
+      state_of_residence: 'CO',
+      spending_money: 10
+    })
+
+    assert_equal('Alexander Aigiades', alexander.full_name)
+  end
 end
