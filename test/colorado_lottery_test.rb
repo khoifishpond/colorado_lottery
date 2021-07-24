f40709bfd865e85b9374aac7c03e4dba73849d9e
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-24 16:11:56 -0700

    Add kind of Hash assertion

--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -13,7 +13,8 @@ class ColoradoLotteryTest < Minitest::Test
 
   def test_it_starts_with_no_registered_contestants
     lottery = ColoradoLottery.new
-    
+
     assert_empty(lottery.registered_contestants)
+    assert_kind_of(Hash, lottery.registered_contestants)
   end
 end
