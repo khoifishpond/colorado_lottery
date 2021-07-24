3f53956bd9889cadf1bb3725ce65db959b0773bc
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-24 16:57:18 -0700

    Create register contestant test

--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -123,4 +123,49 @@ class ColoradoLotteryTest < Minitest::Test
     refute(lottery.can_register?(benjamin, mega_millions))
     refute(lottery.can_register?(frederick, cash_5))
   end
+
+  def test_it_can_register_contestants
+    lottery = ColoradoLottery.new
+    pick_4 = Game.new('Pick 4', 2)
+    mega_millions = Game.new('Mega Millions', 5, true)
+    cash_5 = Game.new('Cash 5', 1)
+    alexander = Contestant.new({
+      first_name: 'Alexander',
+      last_name: 'Aigades',
+      age: 28,
+      state_of_residence: 'CO',
+      spending_money: 10
+    })
+    benjamin = Contestant.new({
+      first_name: 'Benjamin',
+      last_name: 'Franklin',
+      age: 17,
+      state_of_residence: 'PA',
+      spending_money: 100
+    })
+    frederick = Contestant.new({
+      first_name:  'Frederick',
+      last_name: 'Douglas',
+      age: 55,
+      state_of_residence: 'NY',
+      spending_money: 20
+    })
+    winston = Contestant.new({
+      first_name: 'Winston',
+      last_name: 'Churchill',
+      age: 18,
+      state_of_residence: 'CO',
+      spending_money: 5
+    })
+    alexander.add_game_interests('Pick 4')
+    alexander.add_game_interests('Mega Millions')
+    frederick.add_game_interests('Mega Millions')
+    winston.add_game_interests('Cash 5')
+    winston.add_game_interests('Mega Millions')
+    benjamin.add_game_interests('Mega Millions')
+
+    lottery.register_contestant(alexander, pick_4)
+
+    assert_equal({"Pick 4" => alexander})
+  end
 end
