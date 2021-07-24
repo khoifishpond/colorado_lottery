b4814521f5abefbdb3f898a8ae8649bd2a16a18d
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-24 15:07:33 -0700

    Add national drawing method

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,13 @@
 class Game
-  attr_reader :name, :cost, :national_drawing?
+  attr_reader :name, :cost, :national_drawing
 
-  def initialize(name, cost, national_drawing? = false)
+  def initialize(name, cost, national_drawing = false)
     @name = name
     @cost = cost
-    @national_drawing? = national_drawing?
+    @national_drawing = national_drawing
+  end
+
+  def national_drawing?
+    @national_drawing
   end
 end
