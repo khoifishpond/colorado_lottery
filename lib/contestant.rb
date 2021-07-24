397d01f357b048311bc102154ba32992d1ea9d35
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-24 14:54:34 -0700

    Create contestant class

---
+++ lib/contestant.rb
@@ -0,0 +1,11 @@
+class Contestant
+  attr_reader :first_name, :last_name, :age, :state_of_residence, :spending_money
+
+  def initialize(params)
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @age = params[:age]
+    @state_of_residence = params[:state_of_residence]
+    @spending_money = params[:spending_money]
+  end
+end
