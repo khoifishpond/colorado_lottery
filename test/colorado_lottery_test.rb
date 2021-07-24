82ba0f0bc8716925cf81571e8bbe2d0516d64188
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-24 16:24:27 -0700

    Add interested and 18+ test

--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -31,4 +31,46 @@ class ColoradoLotteryTest < Minitest::Test
     assert_empty(lottery.current_contestants)
     assert_kind_of(Hash, lottery.current_contestants)
   end
+
+  def test_it_checks_if_18_or_older
+    pick_4 = Game.new('Pick 4', 2)
+    mega_millions = Game.new('Mega Millions', 5, true)
+    cash_5 = Game.new('Cash 5', 1)
+    alexander = Contestant.new({
+      first_name: 'Alexander',
+      last_name: 'Aigades',
+      age: 28,
+      state_of_residence: 'CO',
+      spending_money: 10
+    })
+    benjamin = Contestant.new({
+      first_name: 'Benjamin',
+      last_name: 'Franklin',
+      age: 17,
+      state_of_residence: 'PA',
+      spending_money: 100
+    })
+    frederick = Contestant.new({
+      first_name:  'Frederick',
+      last_name: 'Douglas',
+      age: 55,
+      state_of_residence: 'NY',
+      spending_money: 20
+    })
+    winston = Contestant.new({
+      first_name: 'Winston',
+      last_name: 'Churchill',
+      age: 18,
+      state_of_residence: 'CO',
+      spending_money: 5
+    })
+    alexander.add_game_interests('Pick 4')
+    alexander.add_game_interests('Mega Millions')
+    frederick.add_game_interests('Mega Millions')
+    winston.add_game_interests('Cash 5')
+    winston.add_game_interests('Mega Millions')
+    benjamin.add_game_interests('Mega Millions')
+
+    assert(lottery.interested_and_18?(alexander, pick_4))
+  end
 end
