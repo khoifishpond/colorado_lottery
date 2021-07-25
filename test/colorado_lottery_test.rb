2d15497dc5a2fb06357bc7438dee8062d31b2d41
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-24 18:44:30 -0700

    Add assertions to test

--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -413,5 +413,20 @@ class ColoradoLotteryTest < Minitest::Test
       },
       lottery.current_contestants
     )
+    assert_equal(14, grace.spending_money)
+    assert_equal(4, winston.spending_money)
+    assert_equal(5, alexander.spending_money)
+    assert_equal(15, frederick.spending_money)
+
+    lottery.charge_contestants(pick_4)
+
+    assert_equal(
+      {
+        cash_5 => ["Winston Churchill", "Grace Hopper"],
+        mega_millions => ["Alexander Aigades", "Frederick Douglas", "Grace Hopper"],
+        pick_4 => ["Alexander Aigades", "Grace Hopper"]
+      },
+      lottery.current_contestants
+    )
   end
 end
