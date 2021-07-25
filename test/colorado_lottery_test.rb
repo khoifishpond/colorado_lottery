4144b2a2c9ac14b4f60fe6689356d1020bbc5247
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-24 19:47:52 -0700

    Add store winners to test

--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -432,7 +432,78 @@ class ColoradoLotteryTest < Minitest::Test
 
   def test_it_can_draw_winners
     lottery = ColoradoLottery.new
-    
+
     assert_equal("2020-04-07", lottery.draw_winners)
+  end
+
+  def test_it_can_store_winners
+    lottery = ColoradoLottery.new
+    pick_4 = Game.new('Pick 4', 2)
+    mega_millions = Game.new('Mega Millions', 5, true)
+    cash_5 = Game.new('Cash 5', 1)
+    alexander = Contestant.new({
+      first_name: 'Alexander',
+      last_name: 'Aigades',
+      age: 28,
+      state_of_residence: 'CO',
+      spending_money: 10
+    })
+    benjamin = Contestant.new({
+      first_name: 'Benjamin',
+      last_name: 'Franklin',
+      age: 17,
+      state_of_residence: 'PA',
+      spending_money: 100
+    })
+    frederick = Contestant.new({
+      first_name:  'Frederick',
+      last_name: 'Douglas',
+      age: 55,
+      state_of_residence: 'NY',
+      spending_money: 20
+    })
+    winston = Contestant.new({
+      first_name: 'Winston',
+      last_name: 'Churchill',
+      age: 18,
+      state_of_residence: 'CO',
+      spending_money: 5
+    })
+    grace = Contestant.new({
+      first_name: 'Grace',
+      last_name: 'Hopper',
+      age: 20,
+      state_of_residence: 'CO',
+      spending_money: 20
+    })
+    alexander.add_game_interests('Pick 4')
+    alexander.add_game_interests('Mega Millions')
+    frederick.add_game_interests('Mega Millions')
+    winston.add_game_interests('Cash 5')
+    winston.add_game_interests('Mega Millions')
+    benjamin.add_game_interests('Mega Millions')
+    grace.add_game_interests('Mega Millions')
+    grace.add_game_interests('Cash 5')
+    grace.add_game_interests('Pick 4')
+    lottery.register_contestant(alexander, pick_4)
+    lottery.register_contestant(alexander, mega_millions)
+    lottery.register_contestant(frederick, mega_millions)
+    lottery.register_contestant(winston, cash_5)
+    lottery.register_contestant(winston, mega_millions)
+    lottery.register_contestant(grace, mega_millions)
+    lottery.register_contestant(grace, cash_5)
+    lottery.register_contestant(grace, pick_4)
+    lottery.charge_contestants(cash_5)
+    lottery.charge_contestants(mega_millions)
+    lottery.charge_contestants(pick_4)
+
+    lottery.announce_winner("Pick 4")
+    lottery.announce_winner("Cash 5")
+    lottery.announce_winner("Mega Millions")
+    
+    assert_kind_of(Array, lottery.winners)
+    assert_kind_of(Hash, lottery.winners.first)
+    assert_kind_of(Hash, lottery.winners.last)
+    assert_equal(3, lottery.winners.length)
   end
 end
