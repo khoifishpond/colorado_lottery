2e6fd0aff8706e7928c25b311b4d5aa92d0f716a
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-24 18:06:14 -0700

    Add charge contestants to test

--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -274,4 +274,68 @@ class ColoradoLotteryTest < Minitest::Test
       lottery.eligible_contestants(mega_millions)
     )
   end
+
+  def test_it_can_charge_contestants
+    lottery = ColoradoLottery.new
+    pick_4 = Game.new('Pick 4', 2)
+    mega_millions = Game.new('Mega Millions', 5, true)
+    cash_5 = Game.new('Cash 5', 1)
+    alexander = Contestant.new({
+      first_name: 'Alexander',
+      last_name: 'Aigades',
+      age: 28,
+      state_of_residence: 'CO',
+      spending_money: 10
+    })
+    benjamin = Contestant.new({
+      first_name: 'Benjamin',
+      last_name: 'Franklin',
+      age: 17,
+      state_of_residence: 'PA',
+      spending_money: 100
+    })
+    frederick = Contestant.new({
+      first_name:  'Frederick',
+      last_name: 'Douglas',
+      age: 55,
+      state_of_residence: 'NY',
+      spending_money: 20
+    })
+    winston = Contestant.new({
+      first_name: 'Winston',
+      last_name: 'Churchill',
+      age: 18,
+      state_of_residence: 'CO',
+      spending_money: 5
+    })
+    grace = Contestant.new({
+      first_name: 'Grace',
+      last_name: 'Hopper',
+      age: 20,
+      state_of_residence: 'CO',
+      spending_money: 20
+    })
+    alexander.add_game_interests('Pick 4')
+    alexander.add_game_interests('Mega Millions')
+    frederick.add_game_interests('Mega Millions')
+    winston.add_game_interests('Cash 5')
+    winston.add_game_interests('Mega Millions')
+    benjamin.add_game_interests('Mega Millions')
+    grace.add_game_interests('Mega Millions')
+    grace.add_game_interests('Cash 5')
+    grace.add_game_interests('Pick 4')
+    lottery.register_contestant(alexander, pick_4)
+    lottery.register_contestant(alexander, mega_millions)
+    lottery.register_contestant(frederick, mega_millions)
+    lottery.register_contestant(winston, cash_5)
+    lottery.register_contestant(winston, mega_millions)
+    lottery.register_contestant(grace, mega_millions)
+    lottery.register_contestant(grace, cash_5)
+    lottery.register_contestant(grace, pick_4)
+
+    lottery.charge_contestants(cash_5)
+
+    assert_equal(19, grace.spending_money)
+    assert_equal(4, winston.spending_money)
+  end
 end
