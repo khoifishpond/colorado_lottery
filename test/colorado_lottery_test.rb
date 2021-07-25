aec18bf214c7196808b34c960cf4d74cf5b16f8b
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-24 17:57:00 -0700

    Add eligible contestants to test

--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -193,7 +193,6 @@ class ColoradoLotteryTest < Minitest::Test
       state_of_residence: 'CO',
       spending_money: 20
     })
-
     grace.add_game_interests('Mega Millions')
     grace.add_game_interests('Cash 5')
     grace.add_game_interests('Pick 4')
@@ -208,5 +207,66 @@ class ColoradoLotteryTest < Minitest::Test
       },
       lottery.registered_contestants
     )
+  end
+
+  def test_it_can_list_eligible_contestants_for_each_game
+    lottery = ColoradoLottery.new
+    pick_4 = Game.new('Pick 4', 2)
+    mega_millions = Game.new('Mega Millions', 5, true)
+    cash_5 = Game.new('Cash 5', 1)
+    alexander = Contestant.new({
+      first_name: 'Alexander',
+      last_name: 'Aigades',
+      age: 28,
+      state_of_residence: 'CO',
+      spending_money: 10
+    })
+    benjamin = Contestant.new({
+      first_name: 'Benjamin',
+      last_name: 'Franklin',
+      age: 17,
+      state_of_residence: 'PA',
+      spending_money: 100
+    })
+    frederick = Contestant.new({
+      first_name:  'Frederick',
+      last_name: 'Douglas',
+      age: 55,
+      state_of_residence: 'NY',
+      spending_money: 20
+    })
+    winston = Contestant.new({
+      first_name: 'Winston',
+      last_name: 'Churchill',
+      age: 18,
+      state_of_residence: 'CO',
+      spending_money: 5
+    })
+    grace = Contestant.new({
+      first_name: 'Grace',
+      last_name: 'Hopper',
+      age: 20,
+      state_of_residence: 'CO',
+      spending_money: 20
+    })
+    alexander.add_game_interests('Pick 4')
+    alexander.add_game_interests('Mega Millions')
+    frederick.add_game_interests('Mega Millions')
+    winston.add_game_interests('Cash 5')
+    winston.add_game_interests('Mega Millions')
+    benjamin.add_game_interests('Mega Millions')
+    grace.add_game_interests('Mega Millions')
+    grace.add_game_interests('Cash 5')
+    grace.add_game_interests('Pick 4')
+    lottery.register_contestant(alexander, pick_4)
+    lottery.register_contestant(alexander, mega_millions)
+    lottery.register_contestant(frederick, mega_millions)
+    lottery.register_contestant(winston, cash_5)
+    lottery.register_contestant(winston, mega_millions)
+    lottery.register_contestant(grace, mega_millions)
+    lottery.register_contestant(grace, cash_5)
+    lottery.register_contestant(grace, pick_4)
+
+    assert_equal([alexander, grace], lottery.eligible_contestants(pick_4))
   end
 end
